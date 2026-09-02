6da27cd3044c5ccb75f66acaec1f31b161419370
Victor Brito <76632500+webdevbynight@users.noreply.github.com>
2026-09-02 16:37:27 +0200

    feat: add release-change bot to valid bots (#60)

--- src/utils/is-valid-bot.ts
+++ src/utils/is-valid-bot.ts
@@ -1,7 +1,7 @@
 import type { GitHubPayload } from "../types.js";
 
-import { GITHUB_ACTIONS_BOT } from "../constants.js";
+import { GITHUB_ACTIONS_BOT, RELEASE_CHANGE_BOT } from "../constants.js";
 
 export const isValidBot = (payload: GitHubPayload): boolean => {
-  return payload.sender.login === GITHUB_ACTIONS_BOT;
+  return payload.sender.login === GITHUB_ACTIONS_BOT || payload.sender.login === RELEASE_CHANGE_BOT;
 };
